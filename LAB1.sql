2a675df516d163db6a4de8bb8b52325db8ba8122
Adarsh Sharma <adarshsharma22220@gmail.com>
2026-06-25 20:45:24 +0530

    second commit

---
+++ LAB1.sql
@@ -0,0 +1,20 @@
+SHOW DATABASES;
+CREATE DATABASE BankingDB;
+USE BankingDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+CREATE TABLE Accounts (
+    AccountID INT PRIMARY KEY,
+    CustomerID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) );
+
